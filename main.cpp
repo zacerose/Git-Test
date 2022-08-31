502280cf3dea564d534e37c1193512f806ce36c6
Zachary Rose <zacerose@ut.utm.edu>
2022-08-31 17:54:55 -0500

    main.cpp now sums all numbers from 1 to a user inputted value

--- main.cpp
+++ main.cpp
@@ -1,10 +1,44 @@
+/**
+ * @file main.cpp
+ * @author Zachary Rose
+ * @date 2022-08-31
+ * @brief sum checker program
+ * 
+ * asks user for a number, and prints the sum of all numbers between it and 1
+ */
+
 
 #include <iostream>
 
 using namespace std;
 
+
+/**
+ * returns the sum of all numbers between 1 and num
+ *
+ * @param int num upper limit of summation
+ * @return int sum of all numbers between 1 and num
+ * 
+ */
+int sum(int num);
+
 int main() {
-  cout << "Hello World!" << endl;
+  int input;
+  cout << "Please enter a number: ";
+  cin >> input;
+
+  cout << "Summed value: " << sum(input) << endl;
   
   return 0;
+}
+
+int sum(int num) {
+  // condition check for peace of mind
+  if (num < 1)
+    return 0;
+  int total = 0;
+  for (int i = 1; i <= num; i++)
+    total += i;
+
+  return total;
 }
